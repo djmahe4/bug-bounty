36347b8e585fcb4fe1f5929aeeb74ba5ed040eca
djmahe4 <maheshwaranup@gmail.com>
2025-03-16 13:49:36 +0530

    Fixed 403_bypass

--- Python/403_bypass.py
+++ Python/403_bypass.py
@@ -1,4 +1,5 @@
-import requests,time
+import requests
+import time
 
 domain=input("Enter domain:")
 #urls=[]
@@ -7,7 +8,8 @@ with open("403_bypass.txt","r") as file:
     print(f"No of urls: {len(urls)}\n Please take rate limit into consideration and edit the code accordingly!\n")
 for url in urls:
     #print(f"https://{domain}/"+"/".join(url.split("/")[1:]))
-    response=requests.get(f"https://{domain}"+"/".join(url.split("/")[1:]))
+    e_url=f"https://{domain}"+"/".join(url.split("/")[1:])
+    response=requests.get(e_url)
     if response.status_code==200:
-        print(f"SUCCESS!: {url}")
+        print(f"SUCCESS!: {e_url}")
     time.sleep(1)
